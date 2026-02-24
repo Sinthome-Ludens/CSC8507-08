169de27430b887c8fd5d40d203125cb931f421a2
CamusY <chienranyu@gmail.com>
2026-02-24 13:50:39 +0000

    Optimize mesh rendering in GameTechRenderer by avoiding redundant back-face pass in wireframe mode.

--- CSC8503/GameTechRenderer.cpp
+++ CSC8503/GameTechRenderer.cpp
@@ -439,16 +439,23 @@ void GameTechRenderer::RenderTransparentPass(std::vector<ObjectSortState>& list)
 		glUniform1i(hasTexLocation, diffuseTex ? 1 : 0);
 	
 		BindMesh((OGLMesh&)*o->GetMesh());
-			
+
 		size_t layerCount = o->GetMesh()->GetSubMeshCount();
 
-		glCullFace(GL_FRONT);
-		for (size_t i = 0; i < layerCount; ++i) {
-			DrawBoundMesh((uint32_t)i);
-		}
-		glCullFace(GL_BACK);
-		for (size_t i = 0; i < layerCount; ++i) {
-			DrawBoundMesh((uint32_t)i);
+		if (m_wireframeMode) {
+			// 线框模式下已禁用背面剔除，单次绘制即可
+			for (size_t i = 0; i < layerCount; ++i) {
+				DrawBoundMesh((uint32_t)i);
+			}
+		} else {
+			glCullFace(GL_FRONT);
+			for (size_t i = 0; i < layerCount; ++i) {
+				DrawBoundMesh((uint32_t)i);
+			}
+			glCullFace(GL_BACK);
+			for (size_t i = 0; i < layerCount; ++i) {
+				DrawBoundMesh((uint32_t)i);
+			}
 		}
 	}
 	glDisable(GL_BLEND);
